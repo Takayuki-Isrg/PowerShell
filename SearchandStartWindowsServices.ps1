57e649f93255ad70adce5431f424ec7f95ee82d3
Takayuki <40379199+Takayuki-Isrg@users.noreply.github.com>
2024-08-21 17:00:07 +0900

    Update SearchandStartWindowsServices.ps1
    １．あいまい検索で、
    複数サービスがヒットした時の選択ロジックを修正
    ２．起動および停止時の確認ロジックを追加
    ３．起動および停止ロジックを1ファイルに統一
    
    1. Refine the selection process for multiple services returned by a fuzzy search query.
    2. Introduce user confirmation dialogs before initiating service start or stop operations.
    3. Merge the functionalities for starting and stopping services into a unified script.

--- SearchandStartWindowsServices.ps1
+++ SearchandStartWindowsServices.ps1
@@ -9,39 +9,71 @@ $ErrorActionPreference = "Continue"
 $services = Get-Service | Select-Object Status, Name, DisplayName
 
 # 検索ワードを入力
-$searchWord = (Read-Host "検索したいWindowsサービス名を入力してください (ワイルドカード可)").ToLower()
+$searchWord = (Read-Host "検索したいWindowsサービス名を入力してください (<検索キーワード>*で部分一致検索)").ToLower()
 
 # 検索結果を取得
 $matches = $services | Where-Object { $_.Name.ToLower() -like $searchWord -or $_.DisplayName.ToLower() -like $searchWord }
 
-# 検索結果がない場合の処理
+# 検索結果がヒットしない場合の処理
 if (-not $matches) {
     Write-Host "該当するWindowsサービスが見つかりません"
     exit
 }
 
-# 複数のWindowsサービスがヒットした場合に起動するWindowsサービスを選択する
+# 複数のWindowsサービスがヒットした場合、起動するWindowsサービスを選択
 if ($matches.Count -gt 1) {
     Write-Host "複数のWindowsサービスがヒットしました。起動するWindowsサービスを選択してください"
     for ($i = 0; $i -lt $matches.Count; $i++) {
         Write-Host "$($i + 1): $($matches[$i].DisplayName)"
     }
-    $index = Read-Host "選択してください:" -as [int]
+    $index = [int](Read-Host "選択してください")
     $serviceToStart = $matches[$index - 1]
 } else {
     $serviceToStart = $matches[0]
 }
 
-# 選択したWindowsサービスが停止状態の場合に起動
+# 停止状態のWindowsサービスを起動する
 if ($serviceToStart.Status -eq "Stopped") {
     try {
-	    Start-Service $serviceToStart.Name
-	    Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' が起動されました"
+        # Windowsサービスを起動するかどうか確認
+        Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' を起動しますか？"
+        $YesOrNo = (Read-Host "(Yes / No)").ToLower()
+
+        # 'Yes'が入力された時の判定を行う
+        if ($YesOrNo.ToLower() -like "*Yes*") {
+            Start-Service $serviceToStart.Name
+            Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' が起動されました"
+        } else {
+            Write-Host "ツールを終了します"
+        }
     } catch {
-	    Write-Host "エラーが発生しました: $($_.Exception.Message)"
-	    Write-Warning "スタックトレース:"
-	    $_.Exception.StackTrace | Out-String
-}
+        Write-Host "エラーが発生しました: $($_.Exception.Message)"
+        Write-Warning "スタックトレース:"
+        $_.Exception.StackTrace | Out-String
+    }
 } else {
     Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' は既に起動しています"
+}
+
+# 起動状態のWindowsサービスを停止する
+if ($serviceToStart.Status -eq "Running") {
+    try {
+        # Windowsサービスを停止するかどうか確認
+        Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' を停止しますか？"
+        $YesOrNo = (Read-Host "(Yes / No)").ToLower()
+
+        # 'Yes'が入力された時の判定を行う
+        if ($YesOrNo.ToLower() -like "*Yes*") {
+            Start-Service $serviceToStart.Name
+            Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' が停止されました"
+        } else {
+            Write-Host "ツールを終了します"
+        }
+    } catch {
+        Write-Host "エラーが発生しました: $($_.Exception.Message)"
+        Write-Warning "スタックトレース:"
+        $_.Exception.StackTrace | Out-String
+    }
+} else {
+    Write-Host "Windowsサービス '$($serviceToStart.DisplayName)' は既に停止しています"
 }
